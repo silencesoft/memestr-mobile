4ee5bf4f4c7141e4841743e481cd73549923415f
Byron H. <bh@axul.net>
2023-10-09 21:41:38 -0500

    Install web libraries. Install react-native-paper library.

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            src: './src',
+          },
+        },
+      ],
+    ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
   };
 };
